136244ae037f52cdc8afcbc8cbf8a5c33d5d7930
Rahul07-dev-ms <iim.rahul.12@gmail.com>
2026-05-14 12:20:12 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool CanAttendMeetings(List<Interval> nums) {
+        var list = nums.OrderBy(x=>x.start);
+        int p = -1, q = -1; 
+        foreach(var arr in list){
+            int x = arr.start;
+            int y = arr.end;
+            if(x < q){
+                return false;
+            }
+            else{
+                p=x;
+                q=y;
+            }
+        }
+        return true;
+    }
+}
